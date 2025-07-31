0feb0adc8a76e4858edf70a038dd92555a1cea8c
Shubham Chamoli <161955389+shubham-chamoli@users.noreply.github.com>
2025-07-31 19:39:02 +0530

    Update 2.c

--- WEEK 1/2.c
+++ WEEK 1/2.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
-int main() {
- // Create a child process
-  pid_t pid = fork();
-  if (pid < 0) {
-   // Fork failed
-   printf("Fork failed!\n");
-   return 1;
-  }
-  else if (pid == 0)
-  {
-    // This is the child process
-    printf("Child Process:\n");
-    printf("PID: %d, Parent PID: %d\n", getpid(), getppid());
-  }
- else {
-   // This is the parent process
-   printf("Parent Process:\n");
-   printf("PID: %d, Child PID: %d\n", getpid(), pid);
-  }
-  return 0;
+int main(){
+    // Create a child process
+    pid_t pid = fork();
+    if (pid < 0){
+        // Fork failed
+        printf("Fork failed!\n");
+        return 1;
+    }
+    else if (pid == 0){
+        // This is the child process
+        printf("Child Process:\n");
+        printf("PID: %d, Parent PID: %d\n", getpid(), getppid());
+    }
+    else{
+        // This is the parent process
+        printf("Parent Process:\n");
+        printf("PID: %d, Child PID: %d\n", getpid(), pid);
+    }
+    return 0;
 }
